cf0d5d6a71379af29233813d300db307ac3a0a10
Gabriele oliveira <131557956+GbNymos@users.noreply.github.com>
2024-09-10 22:32:33 -0300

    aprendendo django

--- django/first_project/tarefas/views.py
+++ django/first_project/tarefas/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 
-lista_tarefas=["estudar","ler","caminhar"]
+
+
+class new_task(forms.Form):
+    task=forms.CharField(label="Nova tarefa: ")
+
 
 def index(request):
+    # verifica se ja existe uma chave informada na sessao
+    if "lista_tarefas" not in request.session:
+        #se nao existe crie uma nova lista
+        request.session["lista_tarefas"]=[]
+
     return render(request,"tarefas/index.html",{
-        "lista":lista_tarefas
+        "lista":request.session["lista_tarefas"]
     })
 
 
 def add(request):
-    return render(request,"tarefas/add.html")
+    if request.method=="POST":
+        form=new_task(request.POST)
+        if form.is_valid():
+            task=form.cleaned_data["task"]
+
+            request.session["lista_tarefas"]+=[task]
+
+            return HttpResponseRedirect(reverse("lista:index"))
+        else:
+            return render(request,"tarefas/add.html",{
+                "form":form
+            })
+    return render(request,"tarefas/add.html",{
+        "form":new_task()
+    })
+
+
+
+
+
+    return render(request,"tarefas/add.html",{
+        "form":new_task()
+    })
